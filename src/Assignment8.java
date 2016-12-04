f39c9a98ec587b25247cdf63756c2cccf09d2ce6
miguelmoran <miguel.moran@gmail.com>
2016-12-04 15:39:08 -0800

    Update Assignment8.java
    Merge Leland's code

--- src/Assignment8.java
+++ src/Assignment8.java
@@ -28,18 +28,26 @@ public class Assignment8 {
 		String command = args[0];
 		
 		System.out.println(">command: "+command);
-		if (command =="resume") 
+		if (command =="resume") {
 			simulator.resume();
-		System.out.println("loading Records");
-		simulator.loadRecords();
+			simulator.loadCache();
+		}
+		else {
+			//create cache files
+			simulator.firstCycle();
+			System.out.println("loading Records");
+			simulator.loadRecords();
+		}
 		
 		System.out.println("loading Instructor Assigments for Semester "+simulator.getCycle());
         boolean moreCycles= simulator.readAssignments();
      
-        if (!moreCycles)
+        if (!moreCycles) {
             System.out.println("> stopping simulation - no more cycles");
-
-        simulator.analizeHistory();
+	    exit = true;
+	}
+	else
+        	simulator.analizeHistory();
 		
 		
 		while (!exit){
@@ -66,12 +74,6 @@ public class Assignment8 {
 			case "show_roster":
 				simulator.showRoster();
 				break;
-			case "show_catalogue":
-				System.out.println("Course Catalogue: ");
-				// call some helper method to display the courses with seats > 0
-				
-				System.out.print("$roster selection >");
-				break;
 		
 			case "submit":
 				System.out.println("selections finalized- now processing requests for Semester "+simulator.getCycle());
@@ -83,6 +85,8 @@ public class Assignment8 {
 				simulator.displayRecords();
 				simulator.displayWaitlist();
 				simulator.incrementWaitlistCycle();
+				simulator.writeCycle();
+				simulator.writeRecords();
 				System.out.print("continue simulation? [yes/no]: ");
 				break;
 				
@@ -93,9 +97,16 @@ public class Assignment8 {
 				scanner.close();
 				break;
 			case "yes":
-				System.out.println("association analysis of previous course selections");
 				simulator.resume();
+				System.out.println("loading Instructor Assigments for Semester "+simulator.getCycle());
+		        moreCycles= simulator.readAssignments();
+				System.out.println("association analysis of previous course selections");
 				simulator.analizeHistory();
+ 
+		        if (!moreCycles){
+		            System.out.println("> stopping simulation - no more cycles");
+		            exit = true;
+		        }
 				
 				break;
 			case "help":			
@@ -104,7 +115,6 @@ public class Assignment8 {
 				System.out.println("help             - show this help screen.");
 				System.out.println("add,row          - add uid from the roster to the assignment.");
 				System.out.println("delete,row       - delete uid from the assignment and put it back on the roster.");
-				System.out.println("show_catalogue   - show the current course catalogue");
 				System.out.println("show_roster      - show the selected and unselected rosters");
 				System.out.println("show_suggestions - show suggestions from WEKA");
 				System.out.println("submit           - submit the selection and complete the instructor assignments.");
